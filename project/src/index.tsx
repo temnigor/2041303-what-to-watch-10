8fc558a56b543d155ea9fb04fcdbd52a7b7e7626
Nikita <temnigor@gmail.com>
2022-08-16 12:40:01 +0400

    от Линтил

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
-  store.dispatch(loadingPageAction(true))
+  store.dispatch(loadingPageAction(true));
   store.dispatch(fetchFilmsActions());
   store.dispatch(fetchFavoriteFilmAction());
   store.dispatch(checkAutAction());
